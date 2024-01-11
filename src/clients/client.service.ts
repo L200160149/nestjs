3bb61c3e9e3c0fc067ef29b6f0752696cb564845
l200160149 <ums.informatika@gmail.com>
2024-01-11 09:09:03 +0700

    add client interface

--- src/clients/client.service.ts
+++ src/clients/client.service.ts
@@ -3,13 +3,24 @@ import { Client } from './interfaces/client.interface';
 
 @Injectable()
 export class ClientService {
-  private readonly clients: Client[] = [];
+    private readonly clients: Client[] = [
+        {
+            id: 1,
+            name: 'Telkomsel',
+            age: 25
+        },
+        {
+            id: 2,
+            name: 'Indosat',
+            age: 30
+        }
+    ];
 
-  create(client: Client) {
-    this.clients.push(client);
-  }
+    create(client: Client) {
+        this.clients.push(client);
+    }
 
-  findAll(): Client[] {
-    return this.clients;
-  }
+    findAll(): Client[] {
+        return this.clients;
+    }
 }
